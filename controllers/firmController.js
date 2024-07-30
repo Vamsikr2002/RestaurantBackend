393326e7477eb0265a5e05694794dadfd0bcd84c
Vamsikr2002 <115055314+Vamsikr2002@users.noreply.github.com>
2024-07-30 14:27:37 +0530

    Update firmController.js

--- controllers/firmController.js
+++ controllers/firmController.js
@@ -4,14 +4,7 @@ const multer = require('multer');
 const path = require('path');
 
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads/'); // Destination folder where the uploaded images will be stored
-    },
-    filename: function(req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname)); // Generating a unique filename
-    }
-});
+
 
 const upload = multer({ storage: storage });
 
@@ -19,15 +12,10 @@ const addFirm = async(req, res) => {
     try {
         const { firmName, area, category, region, offer } = req.body;
 
-        const image = req.file ? req.file.filename : undefined;
 
         const vendor = await Vendor.findById(req.vendorId);
         if (!vendor) {
             res.status(404).json({ message: "Vendor not found" })
-        }
-
-        if (vendor.firm.length > 0) {
-            return res.status(400).json({ message: "vendor can have only one firm" });
         }
 
         const firm = new Firm({
@@ -36,7 +24,6 @@ const addFirm = async(req, res) => {
             category,
             region,
             offer,
-            image,
             vendor: vendor._id
         })
 
