52ce56790d6bfa28021719e28a38e1adda51674e
Vamsikr2002 <115055314+Vamsikr2002@users.noreply.github.com>
2024-07-30 14:29:17 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,21 +4,10 @@ const Firm = require('../models/Firm')
 const path = require('path');
 
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads/'); // Destination folder where the uploaded images will be stored
-    },
-    filename: function(req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname)); // Generating a unique filename
-    }
-});
-
-const upload = multer({ storage: storage });
 
 const addProduct = async(req, res) => {
     try {
         const { productName, price, category, bestSeller, description } = req.body;
-        const image = req.file ? req.file.filename : undefined;
 
         const firmId = req.params.firmId;
         const firm = await Firm.findById(firmId);
@@ -33,7 +22,6 @@ const addProduct = async(req, res) => {
             category,
             bestSeller,
             description,
-            image,
             firm: firm._id
         })
 
@@ -86,4 +74,4 @@ const deleteProductById = async(req, res) => {
     }
 }
 
-module.exports = { addProduct: [upload.single('image'), addProduct], getProductByFirm, deleteProductById };
+module.exports = { addProduct:  addProduct, getProductByFirm, deleteProductById };
